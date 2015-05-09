9aacb294cea13094d7a17d5f7b01031b5e09cd18
IgnacioArroyo <ignacioarroyo699@hotmail.com>
2015-05-09 15:56:34 +0200

    Update Vector2D.cpp

--- Vector2D.cpp
+++ Vector2D.cpp
@@ -1,9 +1,6 @@
 #include "Vector2D.h"
+#include <math.h> 
 
-Vector2D:: ~Vector2D()
-{
-
-}
 
 Vector2D::Vector2D(float xv, float yv)
 {
@@ -11,57 +8,54 @@ Vector2D::Vector2D(float xv, float yv)
 	y = yv;
 }
 
-Vector2D Vector2D::operator+(Vector2D &op)
-{
-	Vector2D resultado;
-	resultado.x = x + op.x;
-	resultado.y = y + op.y;
-	return resultado;
-}
 
-Vector2D Vector2D::operator-(Vector2D &op)
+Vector2D::~Vector2D()
 {
-	Vector2D resultado;
-	resultado.x = x - op.x;
-	resultado.y = y - op.y;
-	return resultado;
-}
 
-Vector2D Vector2D::operator*(float ix)
-{
-	Vector2D resultado;
-	resultado.x = x * ix;
-	resultado.y = y * ix;
-	return resultado;
 }
-
-float Vector2D::operator*(Vector2D &op)
-{
-	float resultado;
-	Vector2D operador;
-	float alpha = (operador.x*op.x + operador.y*operador.x) / (sqrt(operador.x*operador.x + operador.y*operador.y)*sqrt(op.x*op.x + op.y*op.y));
-	resultado = operador.modulo()*op.modulo()*alpha;
-	return resultado;
-}
-
 float Vector2D::modulo()
 {
 	return (float)sqrt(x*x + y*y);
 }
-
 float Vector2D::argumento()
 {
-	return (float)atan2(y,x);
+	return (float)atan2(y, x);
 }
-
-Vector2D Vector2D::unitario()
+Vector2D Vector2D::Unitario()
 {
 	Vector2D retorno(x, y);
 	float mod = modulo();
-	if (mod > 0.00001)
+	if (mod>0.00001)
 	{
 		retorno.x /= mod;
 		retorno.y /= mod;
 	}
 	return retorno;
+}
+Vector2D Vector2D::operator - (const Vector2D &v)const
+{
+	Vector2D res;
+	res.x = x - v.x;
+	res.y = y - v.y;
+	return res;
+}
+
+
+Vector2D Vector2D::operator + (const Vector2D &v)const
+{
+	Vector2D res;
+	res.x = x + v.x;
+	res.y = y + v.y;
+	return res;
+}
+float Vector2D::operator *(const Vector2D &v)	const
+{
+	return x*v.x + y*v.y;
+}
+Vector2D Vector2D::operator *(float f)const
+{
+	Vector2D res;
+	res.x = x*f;
+	res.y = y*f;
+	return res;
 }
