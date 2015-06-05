9f6147a6c0d2d453b6e2c54fb61a102e4c0a2548
IgnacioArroyo <ignacioarroyo699@hotmail.com>
2015-06-05 22:14:33 +0200

    Update Interaccion.cpp

--- Interaccion.cpp
+++ Interaccion.cpp
@@ -8,59 +8,183 @@ Interaccion::~Interaccion()
 {
 }
 
-void Interaccion::Rebote(Rana &h, Caja c,Tronco tronco)
-{
-	Vector2D velocidadnula;
-	h.velocidad = velocidadnula;
+bool Interaccion::Rebote(Rana &h, Plataforma p, Tronco tronco)
+{	
+	float Posicion_x_tronco_min = tronco.posicion.x - tronco.Get_Longitud()/2;
+	float Posicion_x_tronco_max = tronco.posicion.x + tronco.Get_Longitud()/2;
 
-	float xmin = c.linea.limite1.x;
-	float xmax = c.linea.limite4.x;
-	float ymax = c.llegada.limite2.y;
-	float ymin = c.acera.limite1.y;
-	if (h.posicion.x > xmax)h.posicion.x = xmax;
-	if (h.posicion.x < xmin)h.posicion.x = xmin;
-	if (h.posicion.y > ymax)h.posicion.y = ymax;
-	if (h.posicion.y < ymin)h.posicion.y = ymin;
-
-	float ymax_rio=c.rio.limite2.y;
-	float ymin_rio=c.rio.limite1.y;
-	if ((h.posicion.y < ymax_rio) && (h.posicion.y > ymin_rio))
-	{
-		if ((h.posicion.x > tronco.posicion.x - 10) && (h.posicion.x < tronco.posicion.x + 10))
-		{
-			if (tronco.posicion.y == h.posicion.y)
+	//Interaccion de la rana con el tronco
+		if ((h.posicion.y > (tronco.posicion.y-0.5f)) && (h.posicion.y < (tronco.posicion.y+0.5f))&&(h.z==0))
+			if ((h.posicion.x >(Posicion_x_tronco_min)) && (h.posicion.x < (Posicion_x_tronco_max)))
 			{
-				h.velocidad = tronco.velocidad;
+				return true;
 			}
 			else
 			{
-				h.velocidad = velocidadnula;
+				return false;
 			}
-		}
 		else
-			h.posicion = h.origen;
+			{
+				return false;
+			}
+}
+
+bool Interaccion::Destruccion_Vel_Pos(Coche &c, Plataforma caja)
+{
+	// Esto describe los limites de un coche con la caja. DESTRUCCION 
+	float x = caja.acera1.limite4.x+18;
+	if (c.posicion.x > x) return true;
+	else return false;
+}
+
+bool Interaccion::Destruccion_Vel_Neg(Coche &c, Plataforma caja)
+{
+	// Esto describe los limites de un coche con la caja. DESTRUCCION 
+	float x = caja.acera1.limite1.x - 18;
+	if (c.posicion.x < x) return true;
+	else return false;
+}
+
+bool Interaccion::Destruccion_Vel_Pos(Tronco &c, Plataforma caja)
+{
+	// Esto describe los limites de un coche con la caja. DESTRUCCION 
+	float x = caja.acera1.limite4.x + 18;
+	if (c.posicion.x > x) return true;
+	else return false;
+}
+
+bool Interaccion::Destruccion_Vel_Neg(Tronco &c, Plataforma caja)
+{
+	// Esto describe los limites de un coche con la caja. DESTRUCCION 
+	float x = caja.acera1.limite1.x - 18;
+	if (c.posicion.x < x) return true;
+	else return false;
+}
+
+bool Interaccion::Rebote(Rana &r, Coche c)
+{
+	float Posicion_x_coche_min = c.posicion.x - c.Get_Longitud()/2;
+	float Posicion_x_coche_max = c.posicion.x + c.Get_Longitud()/2;
+
+	// Esto describe la colision de la rana con el coche. 
+	if ((r.posicion.x > Posicion_x_coche_min - r.altura / 2) && (r.posicion.x < Posicion_x_coche_max + r.altura / 2))
+	{
+		if (r.posicion.y == c.posicion.y)
+			return true;
+		else return false;
+	}
+	else return false;
+}
+
+bool Interaccion::Rebote(Rana &h, Plataforma p, bool &izq, bool &dch, bool &atr, bool &rec)
+{
+	// Esto describe los limites de la rana con la caja. 
+	float xmin = p.acera1.limite1.x;
+	float xmax = p.acera1.limite3.x;
+	float ymin = 0.0f;
+	float ymax = p.piedra1.limite2.y;
+	if (h.posicion.x > xmax - 0.5)
+	{
+		h.posicion.x = xmax - 0.5;
+		h.z = 0;
+		dch = false;
+	}
+	if (h.posicion.x < xmin + 0.5)
+	{
+		h.posicion.x = xmin + 0.5;
+		h.z = 0;
+		izq = false;
+	}
+	if (h.posicion.y > ymax - 0.5)
+	{
+		h.posicion.y = ymax - 0.5;
+		h.z = 0;
+		rec = false;
+	}
+	if (h.posicion.y < ymin + 0.5)
+	{
+		h.posicion.y = ymin + 0.5;
+		h.z = 0;
+		atr = false;
 	}
 
+	// Esto describe los limites de la rana con el rio y devuelve un booleano para ser tratado.
+	float ymax_rio = p.rio.limite2.y;
+	float ymin_rio = p.rio.limite1.y;
+	if ((h.posicion.y < ymax_rio) && (h.posicion.y > ymin_rio)&&(h.z==0))
+		return true;
+	else return false;
 }
 
-
-void Interaccion::Rebote(Coche &c, Caja caja)
+bool Interaccion::Llegada(Rana &h, Plataforma p, bool &rec)
 {
-	float xmin = caja.linea.limite4.x;
-	if (c.posicion.x > xmin)c.posicion.x = -20.0f;
-}
+	//Esto describe los limites de la rana con los huecos y las piedras
+	float ymin_hueco1 = p.hueco1.limite1.y;
+	float xmin_hueco1 = p.hueco1.limite1.x;
+	float xmax_hueco1 = p.hueco1.limite3.x;
+	float xmin_hueco2 = p.hueco2.limite1.x;
+	float xmax_hueco2 = p.hueco2.limite3.x;
+	float xmin_hueco3 = p.hueco3.limite1.x;
+	float xmax_hueco3 = p.hueco3.limite3.x;
+	float xmin_hueco4 = p.hueco4.limite1.x;
+	float xmax_hueco4 = p.hueco4.limite3.x;
+	float xmin_hueco5 = p.hueco5.limite1.x;
+	float xmax_hueco5 = p.hueco5.limite3.x;
 
-void Interaccion::Rebote(Tronco &t, Caja caja)
-{
-	float xmin = caja.rio.limite2.x;
-	if (t.posicion.x < xmin)t.posicion.x = 20.0f;
-}
+	float xmin_piedra1 = p.piedra1.limite1.x;
+	float xmax_piedra1 = p.piedra1.limite3.x;
+	float xmin_piedra2 = p.piedra2.limite1.x;
+	float xmax_piedra2 = p.piedra2.limite3.x;
+	float xmin_piedra3 = p.piedra3.limite1.x;
+	float xmax_piedra3 = p.piedra3.limite3.x;
+	float xmin_piedra4 = p.piedra4.limite1.x;
+	float xmax_piedra4 = p.piedra4.limite3.x;
+	float xmin_piedra5 = p.piedra5.limite1.x;
+	float xmax_piedra5 = p.piedra5.limite3.x;
+	float xmin_piedra6 = p.piedra6.limite1.x;
+	float xmax_piedra6 = p.piedra6.limite3.x;
 
-void Interaccion::Rebote(Rana &r, Coche c)
-{
-	float dist;
-	Vector2D delta= r.posicion - c.posicion;
-	dist = delta.modulo() - r.altura - c.altura;
-	
-	if (dist <= 0) r.posicion = r.origen;
+	if (h.posicion.y == (ymin_hueco1-0.5f))
+		//HUECOS
+		if ((h.posicion.x > xmin_hueco1) && (h.posicion.x < xmax_hueco1))
+			return true;
+		else if ((h.posicion.x > xmin_hueco2 ) && (h.posicion.x < xmax_hueco2))
+			return true;
+		else if ((h.posicion.x > xmin_hueco3) && (h.posicion.x < xmax_hueco3))
+			return true;
+		else if ((h.posicion.x > xmin_hueco4) && (h.posicion.x < xmax_hueco4))
+			return true;
+		else if ((h.posicion.x > xmin_hueco5) && (h.posicion.x < xmax_hueco5))
+			return true;
+		//PIEDRAS
+		else if ((h.posicion.x > xmin_piedra1 - 0.5) && (h.posicion.x < xmax_piedra1))
+		{
+			h.SetPos(h.GetPos().x, p.hueco1.limite1.y-0.5f);
+			return false;
+		}
+		else if ((h.posicion.x > xmin_piedra2 - 0.5) && (h.posicion.x < xmax_piedra2))
+		{
+			h.SetPos(h.GetPos().x, p.hueco1.limite1.y - 0.5f);
+			return false;
+		}
+		else if ((h.posicion.x > xmin_piedra3 - 0.5) && (h.posicion.x < xmax_piedra3))
+		{
+			h.SetPos(h.GetPos().x, p.hueco1.limite1.y - 0.5f);
+			return false;
+		}
+		else if ((h.posicion.x > xmin_piedra4 - 0.5) && (h.posicion.x < xmax_piedra4))
+		{
+			h.SetPos(h.GetPos().x, p.hueco1.limite1.y - 0.5f);
+			return false;
+		}
+		else if ((h.posicion.x > xmin_piedra5 - 0.5) && (h.posicion.x < xmax_piedra5))
+		{
+			h.SetPos(h.GetPos().x, p.hueco1.limite1.y - 0.5f);
+			return false;
+		}
+		else if ((h.posicion.x > xmin_piedra6 - 0.5) && (h.posicion.x < xmax_piedra6))
+		{
+			h.SetPos(h.GetPos().x, p.hueco1.limite1.y - 0.5f);
+			return false;
+		}
 }
